78d6921948dc120c0a02a8497f5bf04deb1771f8
GNight <tomas11_92@hotmail.com>
2025-02-11 12:42:06 -0500

    C

--- main.go
+++ main.go
@@ -1,12 +1,25 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		input := scanner.Text()
+		lowercased := strings.ToLower(input)
+		words := strings.Fields(lowercased)
+		if len(words) > 0 {
+			fmt.Printf("Your command was: %s\n", words[0])
+
+		}
+	}
 }
 
 func cleanInput(text string) []string {
